7c3a0520a620fa090e5301c66075269dcbcc5e6d
familyboat <2015301110129@whu.edu.cn>
2025-10-02 18:44:37 +0800

    new feature: add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), VitePWA({})],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -15,5 +16,8 @@ export default defineConfig({
   base: './',
   build: {
     outDir: './docs',
+  },
+  server: {
+    port: 5174,
   },
 })
